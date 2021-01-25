48a010b4dbecd39a72a8f640f2b1da7dcd67a5b9
savithanj <76098616+savithanj@users.noreply.github.com>
2021-01-25 12:46:50 +0530

    5.	Write a C program to accept an integer value from the user and display with appropriate message.

---
+++ p1c5.c
@@ -0,0 +1,14 @@
+// 5.	Write a C program to accept an integer value from the user and display with appropriate message.
+
+#include<stdio.h>
+
+int main()
+{
+  int a;
+
+  printf("Enter an integer :");
+  scanf("%d", &a);
+
+  printf("a = %d\n",a);
+  return 0;
+}
