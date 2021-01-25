c4f282165a0c5f05f321da3e29597a130b4684ec
Savitha N <savitha.nj@cmrit.ac.in>
2021-01-25 10:37:40 +0000

    P1

---
+++ P1 C/p1c7.c
@@ -0,0 +1,9 @@
+// 7.	Write a C program to print “Please type “YES” or “NO”:”  message on display using escape sequence in output statement.
+
+#include<stdio.h>
+
+int main()
+{
+	printf("\"Please type \"YES\" or \"NO\":\"");
+	return 0;
+}
