408d2e4527870491f2cd0552cf744ea103dae890
savithanj <76098616+savithanj@users.noreply.github.com>
2021-01-25 12:05:48 +0530

    2.	Write a C program to print your name, today’s date and place.

---
+++ p1c2.c
@@ -0,0 +1,11 @@
+// 2.	Write a C program to print your name, today’s date and place.
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Name: Savitha\n");
+  printf("Date: 25-01-2021\n");
+  printf("Place: Bengaluru\n");
+  return 0;
+}
