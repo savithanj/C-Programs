27d81f96cbf20e8c653145abf132596c3ffb40a0
savithanj <76098616+savithanj@users.noreply.github.com>
2021-01-25 10:31:00 +0530

    14.	Program to find the diameter, area and circumference of the circle

--- area_circum.c
+++ area_circum.c
@@ -1,19 +1,20 @@
+// Program to find the diameter, area and circumference of the circle#in
 #include<stdio.h>
 
 int main()
 {
-  float area, radius, circum;
+  float area, radius, circum, diameter;
 
   printf("Enter radius :");
   scanf("%f", &radius);
 
+  diameter = radius * 2;
   area = 3.14 * radius * radius;
-
   circum = 2 * 3.14 * radius;
 
+  printf("Diameter = %f\n",diameter);
   printf("Area = %f\n", area);
   printf("Circumference = %f\n",circum);
 
   return 0;
 }
-
