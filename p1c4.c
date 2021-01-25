809dea8da2e09c910d00e9c83cd17211950679e1
savithanj <76098616+savithanj@users.noreply.github.com>
2021-01-25 12:42:20 +0530

    4.	Write a C program to print the following using escape sequences.

---
+++ p1c4.c
@@ -0,0 +1,12 @@
+// 4.	Write a C program to print the following using escape sequences.
+
+#include<stdio.h>
+int main()
+{
+  printf("\"I\tam\nan\n\'Engineering\n\tStudent\'\"");
+  // use \t for tab
+  // use \n for newline
+  // use \' for ' (single quote)
+  // use \" for " (double quote)
+  return 0;
+}
