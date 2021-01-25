bba20fd9a3f6a98cc72e4e7018dd8500cd4ea409
savithanj <76098616+savithanj@users.noreply.github.com>
2021-01-25 12:31:03 +0530

    3.	Write a C program to print “Hello! Today is ‘Monday’. Thank You.” using escape sequences.

---
+++ p1c3.c
@@ -0,0 +1,8 @@
+// 3.	Write a C program to print “Hello! Today is ‘Monday’. Thank You.” using escape sequences.
+# include <stdio.h>
+int main()
+{
+  printf("\"Hello! Today is ‘Monday’. Thank You.\"");  
+             // Use \" to print a " (a double quote)
+  return 0;
+}
