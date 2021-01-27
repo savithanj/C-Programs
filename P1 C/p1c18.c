bbb2e808303cad1c0f02e06492d6f210df3a5bad
Savitha N <savitha.nj@cmrit.ac.in>
2021-01-27 04:51:40 +0000

    Write a C program to enter length in centimeters and convert it into meter and print.

---
+++ P1 C/p1c18.c
@@ -0,0 +1,16 @@
+// 18.	Write a C program to enter length in centimeters and convert it into meter and print.
+
+#include<stdio.h>
+int main()
+{
+	float cm,m;
+
+	printf("Enter length in centimeters :");
+	scanf("%f", & cm);
+
+	m = cm / 100.0;
+
+	printf("%f cm = %f m\n",cm,m);
+	
+	return 0;
+}
