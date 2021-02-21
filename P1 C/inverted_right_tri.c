a5fa52329f4c8da6d2f385eef8e4ff275319d75a
Savitha N <savitha.nj@cmrit.ac.in>
2021-02-21 01:21:51 +0000

    Program to print the accepted character as an inverted right triangle for accepted number of rows

--- P1 C/inverted_right_tri.c
+++ P1 C/inverted_right_tri.c
@@ -1,3 +1,11 @@
+/* Program to print the following pattern
+
+*****
+****
+***
+**
+*
+*/
 #include <stdio.h>
 
 int main()
@@ -5,18 +13,18 @@ int main()
 	int n;
 	char ch;
 
-	/*printf("Enter a positive number: ");
+	printf("Enter the number of rows: ");
 	scanf("%d",&n);
 
 	printf("Enter the character to be printed: ");
-	scanf(" %c", &ch);*/
+	scanf(" %c", &ch);
 
 	int i,j;
 	for(i=5;i>=1;i--)
 	{
 		for(j=1;j<=i;j++)
 		{
-			printf("%c",'*');
+			printf("%c",ch);
 		}
 		printf("\n");
 	}
