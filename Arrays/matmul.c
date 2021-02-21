248f1beef58b25a2bf6bac42b08021bca016ad4f
Savitha N <savitha.nj@cmrit.ac.in>
2021-02-21 14:28:12 +0000

    Program to multiply two matrices

--- Arrays/matmul.c
+++ Arrays/matmul.c
@@ -1,23 +1,29 @@
 // Program to illustrate matrix multiplication
+
 #include <stdio.h>
 int main()
 {
 	int m,n,p,q,i,j,k;
 
+	// Accept the order of Matrix A and Matrix B
 	printf("Enter the size of Matrix A :");
 	scanf("%d%d",&m,&n);
 
 	printf("Enter the size of Matrix B :");
 	scanf("%d%d",&p,&q);
 
+	// Check for condition of matrix multiplication
 	if (n != p)
 	{
 		printf("Matrix Multiplication not possible\n");
 		return 1;
 	}
 	
+	// Matrix multiplication is possible
+	// Declare arrays
 	int a[m][n], b[p][q], c[m][q];
 
+	// Initialize Matrix A
 	printf("Enter data into matrix A :");
 	for (i=0;i<m;i++)
 	{
@@ -27,6 +33,7 @@ int main()
 		}
 	}
 
+	// Display Matrix A
 	printf("Matrix A - \n");
 	for (i=0;i<m;i++)
 	{
@@ -37,6 +44,7 @@ int main()
 		printf("\n");
 	}
 	
+	// Initialize Matrix B
 	printf("Enter data into matrix B :");
 	for (i=0;i<p;i++)
 	{
@@ -46,6 +54,7 @@ int main()
 		}
 	}
 
+	// Display Matrix B
 	printf("Matrix b - \n");
 	for (i=0;i<p;i++)
 	{
@@ -56,6 +65,7 @@ int main()
 		printf("\n");
 	}
 	
+	// Multiply Matrix A and Matrix B and save the product in Matrix C
 	for (i=0;i<m;i++)
 	{
 		for(j=0;j<q;j++)
@@ -68,6 +78,7 @@ int main()
 		}
 	}
 	
+	// Display the product matrix
 	printf("The Product matrix is - \n");
 	for (i=0;i<m;i++)
 	{
